c3fa0bc375dd5e8b8869da1af3107ef1f50af605
Diego Lopes <diego-chez@hotmail.com>
2020-06-10 10:14:46 -0300

    sumItems receive the code to sum items

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,16 @@
 import { handleStatus, log } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 
-const sumItems = (items) => {
+const sumItems = (code) => (items) => {
   return items
     .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo === "2143")
+    .filter((item) => item.codigo === code)
     .reduce((total, atual) => total + atual.valor, 0);
 };
 
 document.getElementById("myButton").onclick = () =>
-  fetch("/notas").then(handleStatus).then(sumItems).then(log).catch(log);
+  fetch("/notas")
+    .then(handleStatus)
+    .then(sumItems("2143"))
+    .then(log)
+    .catch(log);
