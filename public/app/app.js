b30f3b5ce5c100b2b0bd3c78d250406976b142eb
Diego Lopes <diego-chez@hotmail.com>
2020-06-10 10:02:07 -0300

    Implement flatmap

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,14 @@
 import { handleStatus, log } from "./utils/promise-helpers.js";
+import "./utils/array-helpers.js";
 
 document.getElementById("myButton").onclick = () =>
   fetch("/notas")
     .then(handleStatus)
-    .then((notas) => notas.reduce((arr, nota) => arr.concat(nota.itens), []))
-    .then((itens) => itens.filter((item) => item.codigo === "2143"))
-    .then((itens) => itens.reduce((total, atual) => total + atual.valor, 0))
+    .then((notas) =>
+      notas
+        .$flatMap((nota) => nota.itens)
+        .filter((item) => item.codigo === "2143")
+        .reduce((total, atual) => total + atual.valor, 0)
+    )
     .then(log)
     .catch(log);
