5be188771110fa7d5a6b91e0d43846db7db793af
Diego Lopes <diego-chez@hotmail.com>
2020-06-10 10:07:57 -0300

    Extract the sum to a function

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,12 @@
 import { handleStatus, log } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 
+const sumItems = (items) => {
+  return items
+    .$flatMap((nota) => nota.itens)
+    .filter((item) => item.codigo === "2143")
+    .reduce((total, atual) => total + atual.valor, 0);
+};
+
 document.getElementById("myButton").onclick = () =>
-  fetch("/notas")
-    .then(handleStatus)
-    .then((notas) =>
-      notas
-        .$flatMap((nota) => nota.itens)
-        .filter((item) => item.codigo === "2143")
-        .reduce((total, atual) => total + atual.valor, 0)
-    )
-    .then(log)
-    .catch(log);
+  fetch("/notas").then(handleStatus).then(sumItems).then(log).catch(log);
