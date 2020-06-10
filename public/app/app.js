4e964880af02b0c6c74b3b9395169185712d31a3
Diego Lopes <diego-chez@hotmail.com>
2020-06-10 09:49:33 -0300

    return the sum of items with code 2143

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,10 @@
-import { handleStatus } from "./utils/promise-helpers.js";
+import { handleStatus, log } from "./utils/promise-helpers.js";
 
 document.getElementById("myButton").onclick = () =>
-  fetch("/notasx")
+  fetch("/notas")
     .then(handleStatus)
-    .then((notas) => console.log(notas))
-    .catch((err) => console.log(err));
+    .then((notas) => notas.reduce((arr, nota) => arr.concat(nota.itens), []))
+    .then((itens) => itens.filter((item) => item.codigo === "2143"))
+    .then((itens) => itens.reduce((total, atual) => total + atual.valor, 0))
+    .then(log)
+    .catch(log);
